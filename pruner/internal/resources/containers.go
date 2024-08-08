56e199b8fa7c82c534fd227f0b8e23e2b57a697f
Said Sef <saidsef@gmail.com>
2024-08-08 13:39:15 +0100

    feat: refactor logging format

--- pruner/internal/resources/containers.go
+++ pruner/internal/resources/containers.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/saidsef/pod-pruner/pruner/utils"
 	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -17,41 +16,78 @@ import (
 // GetContainers retrieves a list of container names from pods in the specified namespace
 // that are in the states defined by the CONTAINER_STATUSES environment variable.
 // It returns a slice of container names in the format "namespace/podName: containerName".
-// If the environment variable is not set or empty, an error is returned.
+// If the environment variable is not set or is empty, an error is returned.
 // If there is an error while listing the pods, it returns an error with context.
+//
+// Parameters:
+// - clientset: A Kubernetes clientset used to interact with the Kubernetes API.
+// - namespace: The namespace from which to retrieve the pods.
+//
+// Returns:
+// - A slice of strings containing the names of the containers in the specified states.
+// - An error if the environment variable is not set, empty, or if there is an error
+// while listing the pods.
 func GetContainers(clientset *kubernetes.Clientset, namespace string) ([]string, error) {
 	statuses := strings.Split(os.Getenv("CONTAINER_STATUSES"), ",")
-	if len(statuses) == 0 {
+	if len(statuses) == 0 || (len(statuses) == 1 && statuses[0] == "") {
 		return nil, fmt.Errorf("CONTAINER_STATUSES environment variable is not set or empty")
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
-	pods, err := clientset.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to list pods in namespace '%s': %w", namespace, err)
-	}
-
 	var containers []string
-	for _, pod := range pods.Items {
-		for _, containerStatus := range pod.Status.ContainerStatuses {
-			if isContainerInState(containerStatus, statuses) {
-				containers = append(containers, fmt.Sprintf("%s/%s: %s", pod.Namespace, pod.Name, containerStatus.Name))
+	var continueToken string
+
+	for {
+		podList, err := clientset.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{
+			Continue: continueToken,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to list pods in namespace '%s': %w", namespace, err)
+		}
+
+		for _, pod := range podList.Items {
+			for _, containerStatus := range pod.Status.ContainerStatuses {
+				if isContainerInState(containerStatus, statuses) {
+					containers = append(containers, fmt.Sprintf("%s/%s: %s", pod.Namespace, pod.Name, containerStatus.Name))
+				}
 			}
 		}
+
+		if podList.Continue == "" {
+			break
+		}
+		continueToken = podList.Continue
 	}
+
 	return containers, nil
 }
 
 // isContainerInState checks if the given container status is in one of the specified states.
 // It returns true if the container is waiting or terminated with a reason that matches one of the statuses.
+//
+// Parameters:
+// - containerStatus: The status of the container to check.
+// - statuses: A slice of strings representing the states to check against.
+//
+// Returns:
+// - A boolean indicating whether the container status matches one of the specified states.
 func isContainerInState(containerStatus v1.ContainerStatus, statuses []string) bool {
-	if containerStatus.State.Waiting != nil && utils.Contains(statuses, containerStatus.State.Waiting.Reason) {
-		return true
+	statusSet := make(map[string]struct{}, len(statuses))
+	for _, status := range statuses {
+		statusSet[status] = struct{}{}
 	}
-	if containerStatus.State.Terminated != nil && utils.Contains(statuses, containerStatus.State.Terminated.Reason) {
-		return true
+
+	if containerStatus.State.Waiting != nil {
+		if _, exists := statusSet[containerStatus.State.Waiting.Reason]; exists {
+			return true
+		}
+	}
+	if containerStatus.State.Terminated != nil {
+		if _, exists := statusSet[containerStatus.State.Terminated.Reason]; exists {
+			return true
+		}
 	}
 	return false
 }
@@ -59,6 +95,12 @@ func isContainerInState(containerStatus v1.ContainerStatus, statuses []string) b
 // DeleteContainers deletes the specified containers (pods) in the given namespace.
 // It logs warnings for any containers that do not conform to the expected format.
 // If a pod deletion fails, it logs an error; otherwise, it logs a success message.
+//
+// Parameters:
+// - clientset: A Kubernetes clientset used to interact with the Kubernetes API.
+// - namespace: The namespace from which to delete the pods.
+// - containers: A slice of strings containing the names of the containers to delete.
+// - log: A logger used to log messages regarding the deletion process.
 func DeleteContainers(clientset *kubernetes.Clientset, namespace string, containers []string, log *logrus.Logger) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
@@ -66,22 +108,34 @@ func DeleteContainers(clientset *kubernetes.Clientset, namespace string, contain
 	for _, container := range containers {
 		parts := strings.Split(container, ": ")
 		if len(parts) != 2 {
-			log.Warnf("Unexpected format for container state: '%s'", container)
+			log.WithFields(logrus.Fields{
+				"state": container,
+				"parts": parts,
+			}).Warn("Unexpected format for container")
 			continue
 		}
 		podInfo := parts[0]
 		podParts := strings.Split(podInfo, "/")
 		if len(podParts) != 2 {
-			log.Warnf("Unexpected format for pod info: '%s'", podInfo)
+			log.WithFields(logrus.Fields{
+				"podInfo": podInfo,
+			}).Warn("Unexpected format for pod info")
 			continue
 		}
 		podName := podParts[1]
 
 		err := clientset.CoreV1().Pods(namespace).Delete(ctx, podName, metav1.DeleteOptions{})
 		if err != nil {
-			log.Errorf("Failed to delete pod '%s' in namespace '%s': %v", podInfo, namespace, err)
+			log.WithFields(logrus.Fields{
+				"pod":       podInfo,
+				"namespace": namespace,
+				"error":     err,
+			}).Error("Failed to delete pod")
 		} else {
-			log.Infof("Successfully deleted pod '%s' in namespace '%s'", podInfo, namespace)
+			log.WithFields(logrus.Fields{
+				"pod":       podInfo,
+				"namespace": namespace,
+			}).Info("Successfully deleted pod")
 		}
 	}
 }
--- pruner/internal/resources/jobs.go
+++ pruner/internal/resources/jobs.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/saidsef/pod-pruner/pruner/utils"
 	"github.com/sirupsen/logrus"
@@ -15,23 +16,27 @@ import (
 // It returns a slice of job descriptions and an error if any occurs.
 //
 // Parameters:
-// - clientset: A Kubernetes clientset used to interact with the Kubernetes API.
+// - clientset: A Kubernetes clientset to interact with the Kubernetes API.
 // - namespace: The namespace from which to retrieve the jobs.
+// - log: A logger to log messages.
 //
 // Returns:
-// - A slice of strings containing descriptions of the jobs that match the specified statuses.
-// - An error if there was an issue retrieving the jobs.
+// - A slice of strings, each representing a job description in the format "namespace/jobName: jobStatus".
+// - An error if any occurs during the retrieval of jobs.
 func GetJobs(clientset *kubernetes.Clientset, namespace string, log *logrus.Logger) ([]string, error) {
-	statuses := strings.Split(utils.GetEnv("JOB_STATUSES", "Complete", log), ",")
-	jobs, err := clientset.BatchV1().Jobs(namespace).List(context.TODO(), metav1.ListOptions{})
+	statuses := strings.Split(strings.TrimSpace(utils.GetEnv("JOB_STATUSES", "Complete", log)), ",")
+	jobs, err := clientset.BatchV1().Jobs(namespace).List(context.Background(), metav1.ListOptions{})
 	if err != nil {
+		log.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("Error retrieving jobs")
 		return nil, err
 	}
 
 	var jobsList []string
 	for _, job := range jobs.Items {
 		for _, jobStatus := range job.Status.Conditions {
-			if utils.Contains(statuses, strings.TrimSpace(fmt.Sprint(jobStatus.Type))) {
+			if utils.Contains(statuses, string(jobStatus.Type)) {
 				jobsList = append(jobsList, fmt.Sprintf("%s/%s: %s", job.Namespace, job.Name, jobStatus.Type))
 			}
 		}
@@ -42,26 +47,37 @@ func GetJobs(clientset *kubernetes.Clientset, namespace string, log *logrus.Logg
 // DeleteJobs deletes the specified jobs from the given namespace and logs the actions taken.
 //
 // Parameters:
-// - clientset: A Kubernetes clientset used to interact with the Kubernetes API.
+// - clientset: A Kubernetes clientset to interact with the Kubernetes API.
 // - namespace: The namespace from which to delete the jobs.
-// - jobs: A slice of job names in the format "namespace/jobName" to be deleted.
-// - log: A logger used to log the actions taken during the deletion process.
-//
-// This function does not return any values.
+// - jobs: A slice of strings, each representing a job description in the format "namespace/jobName: jobStatus".
+// - log: A logger to log messages.
 func DeleteJobs(clientset *kubernetes.Clientset, namespace string, jobs []string, log *logrus.Logger) {
+	var wg sync.WaitGroup
 	for _, job := range jobs {
-		jobParts := strings.Split(job, "/")
-		if len(jobParts) != 2 {
-			log.Errorf("Invalid job format: %s", job)
-			continue
-		}
-		jobName := strings.TrimSpace(strings.Split(jobParts[1], ":")[0]) // Trim any whitespace from the job name
-		propagationPolicy := metav1.DeletePropagationBackground
-		err := clientset.BatchV1().Jobs(namespace).Delete(context.TODO(), jobName, metav1.DeleteOptions{PropagationPolicy: &propagationPolicy})
-		if err != nil {
-			log.Errorf("Failed to delete job %s: %v", jobName, err)
-		} else {
-			log.Infof("Successfully deleted job %s", jobName)
-		}
+		wg.Add(1)
+		go func(job string) {
+			defer wg.Done()
+			jobParts := strings.Split(job, "/")
+			if len(jobParts) != 2 {
+				log.WithFields(logrus.Fields{
+					"job": job,
+				}).Error("Invalid job format")
+				return
+			}
+			jobName := strings.TrimSpace(strings.Split(jobParts[1], ":")[0])
+			propagationPolicy := metav1.DeletePropagationBackground
+			err := clientset.BatchV1().Jobs(namespace).Delete(context.Background(), jobName, metav1.DeleteOptions{PropagationPolicy: &propagationPolicy})
+			if err != nil {
+				log.WithFields(logrus.Fields{
+					"job":   jobName,
+					"error": err,
+				}).Error("Failed to delete job")
+			} else {
+				log.WithFields(logrus.Fields{
+					"job": jobName,
+				}).Info("Successfully deleted job")
+			}
+		}(job)
 	}
+	wg.Wait()
 }
